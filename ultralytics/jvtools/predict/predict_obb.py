580ac5a4f9f1757c58a280ebf8111017ef792baf
JayAi <jyanyong56@163.com>
2024-08-27 11:12:05 +0800

    update 绘制无标签的obb

--- ultralytics/jvtools/predict/predict_obb.py
+++ ultralytics/jvtools/predict/predict_obb.py
@@ -1,8 +1,13 @@
 from ultralytics import YOLO
+from ultralytics.utils.plot_cfg import plot_cfg
 
 # load model
 model = YOLO('/home/pkc/AJ/2024/pythonprojects/ultralytics/weights/best.pt')
 
 # eval
+plot_label = False
+plot_box = False
+plot_mask = True
+plot_cfg(plot_label, plot_box, plot_mask)
 result = model.predict('/home/pkc/AJ/2024/develop/quickTest/assets/wound/after.png',
                        save=True)
